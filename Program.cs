ae5981be0c7efc32c3e2253f4b4e519aa814fb57
greyes97 <gustadolfo.rl@outlook.com>
2020-06-07 17:05:46 -0600

    Primera version 1290-15-3277

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace programa1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           float a = args.Length;
+            for (int i = 0; i < a; i++)
+            {
+                string text = System.IO.File.ReadAllText(@args[i]);
+                text= text.Replace(" ", ",");
+                System.Console.WriteLine("Archivo No. : " +(i+1));
+                System.Console.WriteLine(text+"\n");
+                
+            }
+
+        }
+    }
+}
